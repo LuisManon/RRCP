fb01d102c5902e63e7cfaaee899ea15d9f077067
Luis Mañon <manonluis23@gmail.com>
2024-09-02 22:01:51 -0400

    scroll reveal on the web

---
+++ scrollReveal.js
@@ -0,0 +1,7 @@
+        // Inicializa ScrollReveal
+        ScrollReveal({distance: '60px',duration: 2500,delay: 400});
+        //target elements, and specify options to create reveal animations
+        ScrollReveal().reveal('.effect-bottom', { delay: 500, origin: 'bottom' });
+        ScrollReveal().reveal('.effect-top', { delay: 500, origin: 'top' });
+        ScrollReveal().reveal('.effect-right', { delay: 500, origin: 'right' });
+        ScrollReveal().reveal('.effect-left', { delay: 500, origin: 'left' });
